df15b1a177f603454de24d7c595985eb79242391
Zach Shaffer <zshaffer13@gmail.com>
2025-07-15 15:31:04 -0400

    Init commit of preprocessing

---
+++ dataPlotting.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 15 15:29:14 2025
+
+@author: zshaf
+"""
+
---
+++ dataPreprocessing.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jul 15 11:41:22 2025
+
+@author: zshaf
+"""
+
+import pandas as pd
+from scipy.io import arff
+import ramanspy as rp
+import numpy as np
+
+data = arff.loadarff('./data/tecator.arff')
+df = pd.DataFrame(data[0])
+
+spectral_data = df.iloc[:,0:100]
+pca_data = df.iloc[:,100:122]
+meta_data = df.iloc[:,122:]
+
+pipe = rp.preprocessing.Pipeline([rp.preprocessing.denoise.SavGol(window_length=9, polyorder=3),
+                                 rp.preprocessing.baseline.ASLS(),
+                                 rp.preprocessing.normalise.Vector(pixelwise = True)])
+
+spectral_axis = np.linspace(9523.8,11764.7,100)
+
+raman_spectrum = rp.Spectrum(spectral_data,spectral_axis)
+
+preprocessed_spectra = pipe.apply(raman_spectrum)
+
